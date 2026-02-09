fe4dc7d97bfa301ac7456c9c49bd841d975b5a07
Tarnak Shinde <shindetarnak@gmail.com>
2026-02-09 15:21:28 +0530

    final

--- data.js
+++ data.js
@@ -18,21 +18,21 @@ export const educationData = [
 
 export const projectData = [
   {
-    project_img: "",
+    project_img: "./shopifly.png",
     project_name: "Shopifly",
     project_desc:
       "A Next.js 13+ e-commerce web app built with the App Router, Tailwind CSS, and Supabase. It includes user authentication, a shopping cart, product listings, favorites, and order checkout with email confirmation. Designed as a project template for online shopping experiences",
     project_link: "https://github.com/TarnakShinde/Shopifly",
   },
   {
-    project_img: "/smartbrain.png",
+    project_img: "./smartbrain.png",
     project_name: "SmartBrain",
     project_desc:
       "A simple React website project built with React (showcases robot cards, often with filtering/search) created using React",
     project_link: "https://github.com/TarnakShinde/SmartBrain",
   },
   {
-    project_img: "/robofriends.png",
+    project_img: "./robofriends.png",
     project_name: "RoboFriends",
     project_desc:
       "A React + Node.js face-recognition web app that detects faces in user-submitted images, with features like user sign-in/registration and backend API integration.",
@@ -42,13 +42,13 @@ export const projectData = [
 
 export const certificateData = [
   {
-    certificate_img: "/certificate1.jpg",
+    certificate_img: "./certificate1.jpg",
     certificate_title: "The Complete Web Developer in 2023: Zero-to-mastery ",
     certificate_desc:
       "Completed an intensive full-stack web development program covering HTML, CSS, JavaScript, modern frameworks, backend fundamentals, and real-world projects. Gained practical, hands-on experience in building responsive and scalable web applications.",
   },
   {
-    certificate_img: "/certificate2.jpg",
+    certificate_img: "./certificate2.jpg",
     certificate_title: "Introduction To SQL",
     certificate_desc:
       "Completed foundational training in SQL, focusing on database concepts, writing queries, data retrieval, filtering, and basic database management. Developed essential skills for working with relational databases in real-world applications.",
